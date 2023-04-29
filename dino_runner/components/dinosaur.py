d002a6b2e2bf4aabd8d7f0ad9e621495620ecea1
SebasCz16 <131998160+SebasCz16@users.noreply.github.com>
2023-04-29 16:35:35 -0500

    Class2.homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,9 +1,10 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur:
+    Y_POS_DUCK = 340
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5 
@@ -15,8 +16,8 @@ class Dinosaur:
         self.dino_run = True
         self.step_index = 0
         self.dino_jump = False
-        self.jump_speed = self.JUMP_SPEED  
-           
+        self.jump_speed = self.JUMP_SPEED 
+        self.dino_duck = False
 
     def update (self, user_imput):
         #SI EL DINO ESTA CORRIENDO ES TRUE
@@ -31,12 +32,30 @@ class Dinosaur:
         if self.dino_jump:
             self.jump() 
         
-        if user_imput[pygame.K_UP] and not self.dino_jump:
+        if user_imput[pygame.K_UP] and not self.dino_jump: 
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
+
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        #SI EL DINO ESTA AGACHADO ES TRUE
+        if self.dino_duck:
+            self.duck()
+        
+        elif user_imput[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+
+        elif not (self.dino_jump or user_imput[pygame.K_DOWN]):
+            self.dino_run = True
+            self.dino_jump = False
+            self.dino_duck = False
+            
+            
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
@@ -58,3 +77,11 @@ class Dinosaur:
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
     
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+
+
