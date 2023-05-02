3628628b90998fcfa7ed384ec02229ae2c461ef6
SebasCz16 <131998160+SebasCz16@users.noreply.github.com>
2023-05-02 15:21:14 -0500

    Class 3 Homework

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,21 +1,8 @@
 import random
 import pygame
 from dino_runner.components.obstacles.obstacle import Obstacle
-#from dino_runner.utils.constants import BIRD
 
-class SmallCactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0, 2) # 0, 1, 2
-        super().__init__(image, self.type)
-        self.rect.y = 325 
-
-class LargeCactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0, 2) # 0, 1, 2
-        super().__init__(image, self.type)
-        self.rect.y = 300
-
-'''class Bird_High(Obstacle):
+class Bird_High(Obstacle):
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
@@ -52,4 +39,4 @@ class Bird_Low(Obstacle):
         if self.index >= 9:
             self.index = 0
         screen.blit(self.image[self.index//5], self.rect)
-        self.index += 1'''
+        self.index += 1
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,19 +1,38 @@
+import random
 import pygame
-from dino_runner.components.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.cactus import SmallCactus, LargeCactus
+from dino_runner.components.obstacles.bird import Bird_High, Bird_Low, Bird_Half
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 class ObstacleManager:
     def __init__ (self):
-        self.obtacles = []
+        self.obstacles = []
 
     def update (self, game):
         #Add obstacle to list 
-        if len(self.obtacles) == 0:
-            cactus = Cactus(SMALL_CACTUS)
-            self.obtacles.append(cactus)
+        if len(self.obstacles) == 0:
+            if random.randint(0, 4) == 0:
+                small_cactus = SmallCactus(SMALL_CACTUS)
+                self.obstacles.append(small_cactus)
+            
+            elif random.randint (0, 4) == 1:
+                large_cactus = LargeCactus(LARGE_CACTUS)
+                self.obstacles.append(large_cactus)
+            
+            elif random.randint (0, 4) == 2:
+                bird_one = Bird_High(BIRD)
+                self.obstacles.append(bird_one)
+            
+            elif random.randint (0, 4) == 3:
+                bird_two = Bird_Half(BIRD)
+                self.obstacles.append(bird_two)
+
+            elif random.randint (0, 4) == 4:
+                bird_three = Bird_Low(BIRD)
+                self.obstacles.append(bird_three)
         
-        for obstacle in self.obtacles:
-            obstacle.update(game.game_speed, self.obtacles)
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
           #  pygame.time.delay(100)
            # print(game.player.dino_rect.colliderect(obstacle.rect))
             if game.player.dino_rect.colliderect(obstacle.rect):
@@ -22,5 +41,5 @@ class ObstacleManager:
 
 
     def draw (self, screen):
-        for obstacle in self.obtacles:
+        for obstacle in self.obstacles:
             obstacle.draw(screen)
